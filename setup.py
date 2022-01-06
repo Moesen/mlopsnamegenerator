7ccce8af6e206e9f9b2996e9cdecc9e1572574b9
Gustav Moesmand <gustavmoesmand@gmail.com>
2022-01-06 15:15:25 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Generating new names based on trends in data using GPT2 (Transformer network)',
+    author='Moesen',
+    license='MIT',
+)
