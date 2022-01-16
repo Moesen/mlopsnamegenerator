0e1b7c14b9e952d63a18d9ab277d9a5fbb0f8664
Gustav Moesmand <gustavmoesmand@gmail.com>
2022-01-16 16:08:56 +0100

    added concurrent to download_script

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -84,7 +84,8 @@ def main(cfg: dict):
     val_file = os.path.join(data_folder, "val.csv")
 
     dataset = load_dataset(
-        "csv", data_files={"train": train_file, "validation": val_file},
+        "csv",
+        data_files={"train": train_file, "validation": val_file},
     )
 
     all_descriptions = (
